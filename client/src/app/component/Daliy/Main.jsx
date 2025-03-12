4d84c2bfdad83623180b5f2659146ff00ebc3f0e
unknown <shadab.shaikh.tech@gmail.com>
2025-03-12 15:35:06 +0530

    done done

--- client/src/app/component/Daliy/Main.jsx
+++ client/src/app/component/Daliy/Main.jsx
@@ -6,6 +6,7 @@ import { Tomorrow } from "./Tomorrow";
 import "./loader.css";
 
 import { DailyContext } from "../../Context/DailyContext";
+import { Authcontext } from "../../Context/AuthContext";
 import { Navbar } from "../navbar/Navbar";
 import { Today } from "./Today";
 import { useRouter } from "next/navigation";
