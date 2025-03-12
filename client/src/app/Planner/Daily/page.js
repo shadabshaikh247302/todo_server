bf2f65602615028c0be38ab597e049ec09f4b204
unknown <shadab.shaikh.tech@gmail.com>
2025-03-12 15:26:34 +0530

    done done

--- client/src/app/Planner/Daily/page.js
+++ client/src/app/Planner/Daily/page.js
@@ -4,16 +4,13 @@ import dynamic from 'next/dynamic'
 import React from 'react'
 
 
-const SSRDefault = dynamic(() => import('@/app/component/Daliy/Main'), {
-
-   ssr: false})
+const SSRDefault = dynamic(() => import('../../component/Daliy/Main'), {
+   ssr: false
+})
 
 const Page = () => {
   return (
     <div className='p-3' style={{   background: "linear-gradient(90deg, #BE3144, #1D1616)",height:'100vh'}}>
-      {/* <div style={{margin:'0px',padding:'0px'}}>
-          <Navbar/>
-      </div> */}
       <SSRDefault />
     </div>
   )
--- client/src/app/Planner/Monthly/page.js
+++ client/src/app/Planner/Monthly/page.js
@@ -3,7 +3,7 @@ import dynamic from 'next/dynamic'
 import React from 'react'
 
 // import { Main } from '@/app/component/Monthly/Main'
-const NOSSR=dynamic(()=>import('@/app/component/Monthly/Main'),{ssr:false})
+const NOSSR=dynamic(()=>import('../../component/Monthly/Main'),{ssr:false})
 const page = () => {
   return (
     <div style={{height:"100%", background: "linear-gradient(90deg, #BE3144, #1D1616)"}}>
